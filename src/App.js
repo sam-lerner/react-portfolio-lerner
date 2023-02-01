78ed618f134780fdffe4b6142724b282e27d95bc
Sam Lerner <sam.a.lerner@gmail.com>
2023-02-01 17:00:40 -0500

    added bootstrap link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 
-const App = () => <PortfolioContainer />;
+const styles = {
+  body: {
+      backgroundColor: "rgb(71, 71, 95)",
+      color: "white",
+      fontFamily: "Century Gothic"
+  }
+}
+
+const App = () => {
+  return <div style={styles.body}>
+      <PortfolioContainer />;
+  </div>
+}
 
 export default App;
