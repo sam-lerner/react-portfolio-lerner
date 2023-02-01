de823a06b036216ce563c35391948222d4a3d443
Sam Lerner <sam.a.lerner@gmail.com>
2023-02-01 17:45:44 -0500

    profile photo added

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import profilePhoto from '../../assets/images/sam-lerner.jpg'
 
 const styles = {
   header: {
@@ -15,6 +16,9 @@ const styles = {
       padding: "2vh",
       marginTop: "6vh",
       textAlign: "center"
+  },
+  photo: {
+    imageSize: "20%"
   }
 }
 
@@ -22,6 +26,8 @@ export default function About() {
   return (
     <div style={styles.card}>
       <h1>About Me</h1>
+      <img src={profilePhoto} class="img-fluid" alt="Photo of Sam Lerner" style={{width: '200px'}} ></img>
+      <br></br><br></br>
       <p className= "text-start">
         My name is Sam Lerner and I am a full-stack web developer based in northern New Jersey. After a 20 year career working in Sound Design for theater and live performance, I decided to try something new. While I'm still figuring this coding thing out, I am very proud of my progress so far! Thank you for visiting my portfolio. 
       </p>
